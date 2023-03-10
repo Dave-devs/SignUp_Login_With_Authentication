d080fa04ea3fd5ca1f1fb697d413cfe41420be15
Oreoluwa Odetunde <102271576+LSR07@users.noreply.github.com>
2023-03-10 06:20:16 +0100

    First commit 0.1

---
+++ app/src/main/java/com/dave_devs/signupandloginwithfirebaseauthentication/presentation/navigation/MyAppNavHost.kt
@@ -0,0 +1,22 @@
+package com.dave_devs.signupandloginwithfirebaseauthentication.presentation.navigation
+
+/*@Composable
+fun MyAppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "profile"
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable("profile") {
+            ProfileScreen(
+                onNavigateToFriends = { navController.navigate("friendsList") },
+                /*...*/
+            )
+        }
+        composable("friendslist") { FriendsListScreen(/*...*/) }
+    }
+} */
