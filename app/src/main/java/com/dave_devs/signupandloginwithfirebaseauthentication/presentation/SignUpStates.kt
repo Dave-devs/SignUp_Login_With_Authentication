265b1d409b8280dd2237550d52e74603bcc069bf
Oreoluwa Odetunde <102271576+LSR07@users.noreply.github.com>
2023-03-11 23:14:23 +0100

    First commit 0.4

--- app/src/main/java/com/dave_devs/signupandloginwithfirebaseauthentication/presentation/SignUpStates.kt
+++ app/src/main/java/com/dave_devs/signupandloginwithfirebaseauthentication/presentation/SignUpStates.kt
@@ -4,10 +4,12 @@ data class SignUpStates(
     val nameInput: String = "",
     val emailInput: String = "",
     val passwordInput: String = "",
+    val repeatedPasswordInput: String = "",
     val isInputValid: Boolean = false,
     val isPasswordShown: Boolean = false,
+    val isRepeatedPasswordShown: Boolean = false,
     val errorMessageInput: String? = null,
     val isLoading: Boolean = false,
     val isSuccessfullySignedUp: Boolean = false,
-    val errorMessageLoginProcess: String? = null
+    val errorMessageSignedProcess: String? = null
 )
--- app/src/main/java/com/dave_devs/signupandloginwithfirebaseauthentication/presentation/SignUpViewModel.kt
+++ app/src/main/java/com/dave_devs/signupandloginwithfirebaseauthentication/presentation/SignUpViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.dave_devs.signupandloginwithfirebaseauthentication.domain.use_cases.SignUpAuthenticationUseCase
+import com.dave_devs.signupandloginwithfirebaseauthentication.domain.util.SignUpAuthenticationType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -20,20 +21,65 @@ class SignUpViewModel @Inject constructor(
         signUpState = signUpState.copy(
             nameInput = newValue
         )
+         checkInputValidation()
     }
     fun onEmailInputChange(newValue: String){
         signUpState = signUpState.copy(
             emailInput = newValue
         )
+         checkInputValidation()
     }
     fun onPasswordInput(newValue: String) {
         signUpState = signUpState.copy(
             passwordInput = newValue
         )
+         checkInputValidation()
+    }
+    fun onRepeatedPasswordInput(newValue: String) {
+        signUpState = signUpState.copy(
+            repeatedPasswordInput = newValue
+        )
+        checkInputValidation()
     }
     fun onToggleVisibilityTransformation() {
         signUpState = signUpState.copy(
-            isPasswordShown = !signUpState.isPasswordShown
+            isPasswordShown = !signUpState.isPasswordShown,
+            isRepeatedPasswordShown = !signUpState.isRepeatedPasswordShown
         )
+    }
+    private fun checkInputValidation() {
+        val validationResult = signUpAuthenticationUseCase(
+            signUpState.nameInput,
+            signUpState.emailInput,
+            signUpState.passwordInput,
+            signUpState.repeatedPasswordInput
+        )
+        processInputValidationType(validationResult)
+    }
+
+    private fun processInputValidationType(type: SignUpAuthenticationType) {
+        signUpState = when(type) {
+            is SignUpAuthenticationType.Valid -> {
+                signUpState.copy(errorMessageInput = null, isInputValid = true )
+            }
+            is SignUpAuthenticationType.EmptyField -> {
+                signUpState.copy(errorMessageInput = "An empty field left", isInputValid = false)
+            }
+            is SignUpAuthenticationType.InvalidEmail -> {
+                signUpState.copy(errorMessageInput = "An invalid email", isInputValid = false)
+            }
+            is SignUpAuthenticationType.PasswordTooSort -> {
+                signUpState.copy(errorMessageInput = "Password is less than 10 characters", isInputValid = false)
+            }
+            is SignUpAuthenticationType.PasswordSpecialCharMissing -> {
+                signUpState.copy(errorMessageInput = "Password must contain at least one special character", isInputValid = false)
+            }
+            is SignUpAuthenticationType.PasswordUpperCaseMissing -> {
+                signUpState.copy(errorMessageInput = "Password must contain at least one capital letter", isInputValid = false)
+            }
+            is SignUpAuthenticationType.PasswordDoNotMatch -> {
+                signUpState.copy(errorMessageInput = "Password do not match", isInputValid = false)
+            }
+        }
     }
 }
